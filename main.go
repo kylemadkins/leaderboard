b0b5267d25a950a09f6a1a0558fdb7e17a0d1f02
kylemadkins <kyle.m.adkins@gmail.com>
2023-07-27 14:27:43 -0400

    Simplify listen and serve logic

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"net"
 	"net/http"
 	"os"
 	"os/signal"
@@ -18,11 +17,6 @@ import (
 func main() {
 	addr := "8000"
 
-	listener, err := net.Listen("tcp", fmt.Sprintf(":%s", addr))
-	if err != nil {
-		log.Fatalf("Error: %s", err.Error())
-	}
-
 	database, err := db.Initialize(os.Getenv("DATABASE_URL"))
 	if err != nil {
 		log.Fatalf("Failed to set up database: %s", err.Error())
@@ -30,25 +24,23 @@ func main() {
 	defer database.Conn.Close()
 
 	httpHandler := handlers.NewHandler(database)
-	server := &http.Server{
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", addr),
 		Handler: httpHandler,
 	}
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
 	go func() {
-		server.Serve(listener)
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf(err.Error())
+		}
 	}()
-	defer Stop(server)
-	log.Printf("Listening on port %s...", addr)
-	ch := make(chan os.Signal, 1)
-	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
-	log.Println(fmt.Sprint(<-ch))
-	log.Println("Stopping API server...")
-}
-
-func Stop(server *http.Server) {
+	log.Printf("Server started on port %s\n", addr)
+	<-done
+	log.Println("Exiting server")
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
-	if err := server.Shutdown(ctx); err != nil {
-		log.Printf("Unable to shut server down correctly: %s\n", err.Error())
-		os.Exit(1)
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatalf("Failed to exit: %+v", err)
 	}
 }
